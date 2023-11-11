cdc124be1a7cbae5424229c8c348398c5db8ef4e
Kotlyarova04 <p.kotlyarova@g.nsu.ru>
2023-11-11 22:46:57 +0700

    aaaaaaa

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ with open('input.txt', 'r', encoding='utf-8') as inp:
     text = inp.read()
     text = text.split()
 word_dict = {}
-for word in range(len(text)):
-    if text[word] not in word_dict:
-        word_dict[text[word]] = 1
+for word in text:
+    if word not in word_dict:
+        word_dict[word] = 1
+    unique = []
+    for words in text[text.index(word)+1:]:
+        if words != word:
+            if words not in unique:
+                unique.append(words)
+    word_dict[word] = unique
+
+
 print(word_dict)
