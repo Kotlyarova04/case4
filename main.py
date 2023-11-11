d93b7e29bae0fb1669695fe8bae9366d752d6f35
Kotlyarova04 <p.kotlyarova@g.nsu.ru>
2023-11-11 21:02:58 +0700

    aaaaaaa

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with open('input.txt', 'r', encoding='utf-8') as inp:
+    text = inp.read()
+    text = text.split()
+word_dict = {}
+for word in range(len(text)):
+    if text[word] not in word_dict:
+        word_dict[text[word]] = 1
+print(word_dict)
