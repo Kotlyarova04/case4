c2eb05c21cff30e62e35027426f6474723097cf0
Kotlyarova04 <p.kotlyarova@g.nsu.ru>
2023-11-12 13:34:07 +0700

    aaaaaaa

--- main.py
+++ main.py
@@ -1,17 +1,3 @@
-'''with open('input.txt', 'r', encoding='utf-8') as inp:
-    text = inp.read()
-    text = text.split()
-word_dict = {}
-for word in text:
-    if word not in word_dict:
-        unique = []
-        for words in text[text.index(word)+1:]:
-            if words != word:
-                if words not in unique:
-                    unique.append(words)
-        word_dict[word] = unique
-
-print(word_dict)'''
 import random
 
 with open('input.txt', 'r', encoding='utf-8') as inp:
